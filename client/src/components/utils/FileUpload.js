ffc1cc0047d6fba3d9a2e1167f285a7da21949c8
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-03-22 12:38:09 +0000

    Dropzone dep installed working, creating add and delete function for images

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Icon } from 'antd';
+import Dropzone from 'react-dropzone';
 
 function FileUpload() {
     return (
-        <div>
-            FileUpload 
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+            <Dropzone
+            onDrop
+            multiple
+            maxSize
+            >
+                {({getRootProps, getInputProps}) => (
+                    <div style={{width:'300px', height: '240px', border: '1px solid lightgrey', display: 'flex', alignItems: 'center' }}
+                    {...getRootProps()}
+                    >
+                        <input {...getInputProps()} />
+                        <Icon type="plus" style={{fontSize: '3em'}} />
+
+                    </div>
+                )}
+
+
+
+            </Dropzone>
+
+
+
         </div>
     )
 }
